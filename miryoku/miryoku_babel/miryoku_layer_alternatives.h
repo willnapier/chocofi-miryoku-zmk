72ba556a35b14f9faf4bb27595af16b75335a71f
Will Napier <will@willnapier.com>
2025-06-02 23:09:47 +0100

    Update MIRYOKU_ALTERNATIVES_MOUSE to match default Miryoku mouse layer with scroll buttons

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -304,11 +304,10 @@ U_STU,             &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LCTRL,          &kp LALT,          &kp LGUI,         &kp LSHFT,         U_NA,              U_NU,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
-U_STU,             &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_NU,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
-
+&none &none &none &none &none   &none &none &none &none &none \
+&kp LCTRL &kp LALT &kp LGUI &kp LSHFT &none   &none U_MS_L U_MS_D U_MS_U U_MS_R \
+&none &kp RALT &none &none &none   &none U_WH_L U_WH_D U_WH_U U_WH_R \
+&none &none &none &none U_BTN1   U_BTN2 U_BTN1 U_BTN3 &none &none
 
 #define MIRYOKU_ALTERNATIVES_MEDIA_INVERTEDT_FLIP \
 U_RGB_HUI,         U_RGB_SAI,         &kp C_VOL_UP,      U_RGB_BRI,         U_RGB_TOG,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
